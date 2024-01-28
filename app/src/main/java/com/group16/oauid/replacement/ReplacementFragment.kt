93ae96c8f811e13a59b4791fac39b16f859a37db
Oluwafemi <ojurifemi132@gmal.com>
2024-01-28 22:53:07 +0100

    impl

---
+++ app/src/main/java/com/group16/oauid/replacement/ReplacementFragment.kt
@@ -0,0 +1,19 @@
+package com.group16.oauid.replacement
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.group16.oauid.BaseFragment
+import com.group16.oauid.R
+
+class ReplacementFragment: BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_replacement, container, false)
+    }
+}
